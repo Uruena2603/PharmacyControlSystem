fd452ebcea64a83d15fdb24a2c7fc6e4a18dc7eb
Uruena2603 <alejourus2003@gmail.com>
2023-11-30 21:16:12 -0500

    Working On Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,13 +6,40 @@
 #include "Medicamento.h"
 using namespace std;
 
+void limpiarDatos()
+{
+}
+
+void menu()
+{
+    int menu = 0;
+    do
+    {
+        system("cls");
+        cout << "BIENVENIDO PHARMACY CONTROL SYSTEM\n";
+        cout << "ADMINISTRAR MEDICAMENTOS------->(1)\n";
+        cout << "ADMINISTRAR LABORATORIOS------->(2)\n";
+        cout << "ADMINISTRAR CLIENTES----------->(3)\n";
+        cout << "COMPRAR MEDICAMENTOS----------->(4)\n";
+        cout << "VENDER MEDICAMENTOS------------>(5)\n";
+        cout << "HISTORIAL FACTURAS------------->(6)\n";
+        cout << "CONSULTAS---------------------->(7)\n";
+        cout << "CERRAR PROGRMAMA--------------->(7)\n";
+
+        cin >> menu;
+
+        switch (menu)
+        {
+        }
+
+    } while (menu >= 1 && menu <= 8);
+}
+
 int main()
 {
     system("cls");
-    pruebaCliente();
-    pruebaCompraVenta();
-    pruebaConsulta();
-    pruebaLaboratorio();
-    pruebaMedicamento();
+    limpiarDatos();
+    menu();
+    cin.ignore();
     return 0;
 }
--- Medicamento.h
+++ Medicamento.h
@@ -3,6 +3,30 @@
 #ifndef MEDICAMENTO_H
 #define MEDICAMENTO_H
 using namespace std;
+
+enum tipoMedicamento
+{
+    Sertralina,
+    Fluoxetina,
+    Escitalopram,
+    Venlafaxina,
+    Paroxetina,
+    Duloxetina,
+    Citalopram,
+    Bupropión,
+    Mirtazapina,
+    Tranilcipromina
+};
+
+struct medicamento
+{
+    int codigoMedicamento;
+    string nombreMedicamento;
+    int stockMin;
+    int unidadesExistentes;
+    tipoMedicamento tipo;
+};
+
 void pruebaMedicamento()
 {
     cout << "Importando Medicamento\n";

07ff9b941487af7d3aa29dd2b6865385a4f83159
Uruena2603 <alejourus2003@gmail.com>
2023-11-30 19:27:33 -0500

    Testing Modules

---
+++ Laboratorio.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void prueba(){
+    cout<<"Prueba Importación";
+}
+
+int main(){
+  
+    return 0;
+}
--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<Laboratorio.h>
 using namespace std;
 
 int main(){
     cout<<"otro commit\n";
+    prueba();
     return 0;
 }
